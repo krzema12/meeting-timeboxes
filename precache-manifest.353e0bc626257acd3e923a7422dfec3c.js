b071f31906a46def91b4c6d43489c65d74b98708
Piotr Krzeminski <git@krzeminski.it>
2019-02-12 21:02:34 +0100

    Updates

---
+++ precache-manifest.353e0bc626257acd3e923a7422dfec3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52d3c17a79e8e45bed7f",
+    "url": "/meeting-timeboxes/static/js/runtime~main.52d3c17a.js"
+  },
+  {
+    "revision": "b14939ebcd9b59ed835e",
+    "url": "/meeting-timeboxes/static/js/main.b14939eb.chunk.js"
+  },
+  {
+    "revision": "bb4bb0dfae2fb2992aef",
+    "url": "/meeting-timeboxes/static/js/1.bb4bb0df.chunk.js"
+  },
+  {
+    "revision": "b14939ebcd9b59ed835e",
+    "url": "/meeting-timeboxes/static/css/main.63fda6a1.chunk.css"
+  },
+  {
+    "revision": "bb4bb0dfae2fb2992aef",
+    "url": "/meeting-timeboxes/static/css/1.1352ed9b.chunk.css"
+  },
+  {
+    "revision": "dba4a3dd2bff89b304ae8975d60ddeed",
+    "url": "/meeting-timeboxes/index.html"
+  }
+];
